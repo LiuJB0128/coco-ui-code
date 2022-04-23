718364d8358fd90e8faf478046a5ab6847ee10ee
LiuJB0128 <1178570567@qq.com>
2022-04-23 19:00:21 +0800

    Switch 按钮完成

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    Switch 的文档
+    <Switch v-model:value="bool"/>
   </div>
 </template>
+<script lang="ts">
+import Switch from '../lib/Switch.vue';
+import {ref} from 'vue';
+
+export default {
+  components: {Switch},
+  setup() {
+    const bool = ref(false);
+    return {
+      bool
+    };
+  }
+};
+</script>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <button class="coco-switch" @click="toggle" :class="{'coco-checked':value}">
+      <span/>
+    </button>
+  </div>
+</template>
+<script lang="ts">
+
+export default {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    const toggle = () => {
+      context.emit('update:value', !props.value);
+    };
+    return {toggle};
+  }
+};
+</script>
+<style lang="scss">
+@use "sass:math";
+
+$h: 22px;
+$h2: $h - 4px;
+.coco-switch {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: math.div($h, 2);
+  position: relative;
+
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: math.div($h2, 2);
+    transition: all 250ms;
+  }
+
+  &.coco-checked {
+    background: #1890ff;
+
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+
+  &.coco-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
+  }
+}
+</style>
