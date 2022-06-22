f5605774755d11da51f4ad1db3eb1ba2666b7cfa
LiuJB0128 <1178570567@qq.com>
2022-06-22 12:52:27 +0800

    支持 markdown 文件

--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -1,3 +1,16 @@
 <template>
-  GetStarted
+  <article class="markdown-body" v-html="md">
+  </article>
 </template>
+
+<script lang="ts">
+import md from '../markdown/get-started.md'
+
+export default {
+  data() {
+    return {
+      md
+    }
+  }
+}
+</script>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,3 +1,16 @@
 <template>
-  intro
+  <article class="markdown-body" v-html="md">
+  </article>
 </template>
+
+<script lang="ts">
+import md from '../markdown/intro.md'
+
+export default {
+  data() {
+    return {
+      md
+    }
+  }
+}
+</script>
