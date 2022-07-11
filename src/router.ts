d1b216af391cf8f648375f4e72f1f74e93b845f4
LiuJB0128 <1178570567@qq.com>
2022-07-11 14:41:23 +0800

    解决 build 之后加载 .md 文件报错

--- src/router.ts
+++ src/router.ts
@@ -6,10 +6,16 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import {h} from 'vue';
-import Markdown from './components/Markdown.vue'
+import Markdown from './components/Markdown.vue';
+// @ts-ignore
+import intro from './markdown/intro.md';
+// @ts-ignore
+import getStarted from './markdown/get-started.md';
+// @ts-ignore
+import install from './markdown/install.md';
 
 const history = createWebHashHistory();
-const md = filename => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
+const md = string => h(Markdown, {content: string, key: string});
 export const router = createRouter({
   history: history,
   routes: [
@@ -18,9 +24,9 @@ export const router = createRouter({
       path: '/doc', component: Doc,
       children: [
         {path: '', redirect: '/doc/intro'},
-        {path: 'intro', component: md('intro')},
-        {path: 'install', component: md('install')},
-        {path: 'get-started', component: md('get-started')},
+        {path: 'intro', component: md(intro)},
+        {path: 'install', component: md(install)},
+        {path: 'get-started', component: md(getStarted)},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
